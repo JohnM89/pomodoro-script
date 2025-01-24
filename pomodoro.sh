ef679c4815e9f4e25a6f233557713087b4634f08
JohnM89 <mjammer2@gmail.com>
2025-01-24 14:25:40 -0500

    changed command line output rows

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,29 +1,32 @@
 #! /bin/bash
 
+#clear terminal
+clear
 #store arguments with $num
-
+# -z check if empty
 if [[ -z $1 ]]; then
   echo "No focus topic passed"
+  #exit
 else
-  echo "25min to focus on ($1)!"
+  echo "--- Focus on ($1)! ---"
 fi
 
 #simple tracking of tasks
-echo $1 >>.pomodorotasks
-#clear terminal
-clear
+# -n check if NOT empty and pass task to task tracking text
+[[ -n "$1" ]] && echo $1 >>.pomodorotasks
 #set variables for animation
 val="_"
 val2="-"
 len1=0
 len2=16
-#amount=1
 max_length=16
 study=1500
 switch=0
 #break=300
 #hide the cursor
 echo -ne "\e[?25l"
+#add trap handler for user pressing SIGNIT so cursor shows back up
+trap 'echo -ne "\e[?25h\n"; exit' SIGINT SIGTERM
 
 #for loop to handle countdown
 #echo with \033[<row>;<col>H for carraiage returns allows updating the same line
@@ -49,15 +52,14 @@ for ((i = 0; i < study; i++)); do
   line1=$(printf "%${len1}s" | tr " " "$val")
   line2=$(printf "%${len2}s" | tr " " "$val2")
 
-  echo -ne "\033[1;0HTime left: $(date -u --date="@${time}" +%M:%S)"
-  #echo -ne "\033[2;0H\033[K"
-  echo -ne "\033[2;0H$line1$line2"
-  #echo -ne "$len1\t"
+  echo -ne "\033[2;0HTime left: $(date -u --date="@${time}" +%M:%S)"
+  echo -ne "\033[3;0H$line1$line2"
   sleep 1
 done
 
 #play an alert
 echo -e "\a"
+#printf "\a"
 
 echo "Times up!"
 #show cursor again
